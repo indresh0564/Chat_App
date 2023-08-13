9236acbf5118b8357156788671f9036e0e122251
indresh0564 <indresh0564@gmail.com>
2023-08-13 19:32:38 +0530

    login page

--- backend/controller/signup.js
+++ backend/controller/signup.js
@@ -2,28 +2,36 @@ const { json } = require('body-parser');
 const user_model = require('../models/user');
 const bcrypt = require('bcrypt');
 
-const User = (req, res)=>{
+const User = async(req, res)=>{
         try{
             const {name , email, contact , password } = req.body;
-            const saltroud=10;
-            bcrypt.hash(password , saltroud , async(err , hash_pass)=>{
-                if(err){
-                    throw new Error(JSON.stringify(err));
-                }
-    
-                await user_model.create({
-                    name:name,
-                    email:email,
-                    contact:contact,
-                    password:hash_pass
-                })
-                .then((data)=>{
-                    res.status(201).json({newUser:data});
-                })
-                .catch((err)=>{
-                    throw new Error(JSON.stringify(err));
-                })
-            })
+
+           const data = await user_model.findOne({where:{email:email}})
+            if(data){
+                res.status(201).json({message:" this user already exist "});
+            }else{
+                const saltroud=10;
+                bcrypt.hash(password , saltroud , async(err , hash_pass)=>{
+                    if(err){
+                        throw new Error(JSON.stringify(err));
+                    }
+        
+                    await user_model.create({
+                        name:name,
+                        email:email,
+                        contact:contact,
+                        password:hash_pass
+                    })
+                    .then((data)=>{
+                        res.status(200).json({message:"Succesfully Resister", newUser:data});
+                    })
+                    .catch((err)=>{
+                        throw new Error(JSON.stringify(err));
+                    })
+                }) 
+            }           
+            
+            
         }catch(err){
             console.log(err);
         }  
